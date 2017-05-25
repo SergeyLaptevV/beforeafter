8a13a95a25dd528c0e8919967ddc65a2afdfcfd4
Laptev <svet-smeh12@ukr.net>
2017-05-25 13:55:27 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function(){
+	$('.Photo1').beforeAfter({
+        animateIntro : true,
+        showFullLinks : false,
+
+        dividerColor : '#7bb72d'
+    });
+    $('.Photo2').beforeAfter({
+        animateIntro : true,
+        showFullLinks : false,
+        dividerColor : '#7bb72d'
+    });
+    $('.Photo3').beforeAfter({
+        animateIntro : true,
+        showFullLinks : false,
+        dividerColor : '#7bb72d'
+    });
+});
